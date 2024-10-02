d56c21aa18ce505ab33640998cee3ab7856df33b
Amandeep Singh <amandeep@192.168.1.10>
2024-10-02 22:40:23 +0530

    Added indexes

---
+++ database/migrations/2024_10_03_030914_add_indexes_to_shopify_product_variants.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('shopify_product_variants', function (Blueprint $table) {
+            $table->index('price_requires_update');
+            $table->index('inventory_requires_update');
+            $table->index('images_requires_update');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('shopify_product_variants', function (Blueprint $table) {
+            //
+        });
+    }
+};
