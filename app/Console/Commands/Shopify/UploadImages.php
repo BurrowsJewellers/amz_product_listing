09670ae66a1f7d7efaf460961c75bc03c33acfe4
Amandeep Singh <amandeep@192.168.1.10>
2024-10-03 10:37:24 +0530

    Upload images

--- app/Console/Commands/Shopify/UploadImages.php
+++ app/Console/Commands/Shopify/UploadImages.php
@@ -48,6 +48,68 @@ class UploadImages extends Command
                 $this->info("Remaining {$count}");
 
                 while ($count) {
+                    if ($variant = ShopifyProductVariant::where('images_requires_update', 1)->with(['images', 'product'])->first()) {
+                        $this->info("Uploading images for {$variant->sku}");
+                        if ($variant->images->count()) {
+                            foreach ($variant->images as $i) {
+                                try {
+                                    $image = new Image($session);
+                                    $image->product_id = $variant->product_id;
+                                    $image->src = $i->url;
+                                    $image->variant_ids = [
+                                        $variant->variant_id
+                                    ];
+
+                                    $image->save(
+                                        true, // Update Object
+                                    );
+                                    $this->info("Image uploaded for sku {$variant->sku}, variant id  {$variant->variant_id}");
+                                    $variant->update(['images_requires_update' => 0]);
+                                } catch (\Exception $e) {
+                                    Log::debug("There was an error while uploading the images for {$variant->sku}. Error message : {$e->getMessage()}");
+                                    $variant->update(['images_requires_update' => 2]);
+                                }
+                            }
+                        }
+                    } else {
+                        $this->error("No variant found with images_requires_update = 1");
+                    }
+
+                    $count = ShopifyProductVariant::where('images_requires_update', 1)->count();
+                    $this->info("Remaining {$count}");
+                }
+
+                $job->update(['status' => 0]);
+
+                Log::info("$marketplace $jobType finished!");
+            } catch (\Exception $e) {
+                $job->update(['status' => 0, 'message' => $e->getMessage()]);
+                report($e);
+                $this->error($e->getMessage());
+            }
+        } else {
+            Log::info("$marketplace $jobType is already running.");
+        }
+    }
+
+
+    public function handleBackup()
+    {
+        $marketplace = 'Shopify';
+        $jobType = 'uploadImages';
+
+        $job = SyncJobController::getJob($jobType, $marketplace);
+
+        if (!$job->isRunning()) {
+            try {
+                Log::info("$marketplace $jobType started!");
+                $job->update(['status' => 1]);
+                $session = (new ShopifyService)->getSession();
+
+                $count = ShopifyProductVariant::where('images_requires_update', 1)->count();
+                $this->info("Remaining {$count}");
+
+                while ($count) {
                     $variant = ShopifyProductVariant::where('images_requires_update', 1)->with(['images', 'product'])->first();
 
                     if (!$variant) {
