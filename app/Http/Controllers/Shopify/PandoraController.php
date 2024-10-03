bdd4a515c5ce22dc5b465f052a7cf494e6a98230
Amandeep Singh <amandeep@192.168.1.10>
2024-10-03 15:49:31 +0530

    Updates

--- app/Http/Controllers/Shopify/PandoraController.php
+++ app/Http/Controllers/Shopify/PandoraController.php
@@ -11,7 +11,13 @@ class PandoraController extends Controller
     public function uploadData(Request $request)
     {
         try {
-            return (new ShopifyService)->uploadImages($request);
+            (new ShopifyService)->uploadImages($request);
+
+            return response()
+                ->json(['message' => 'Data uploaded successfully'])
+                ->header('Access-Control-Allow-Origin', '*') // or specify a specific domain
+                ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With');
         } catch (\Exception $e) {
             report($e);
             return response()->json('failed', 500);
