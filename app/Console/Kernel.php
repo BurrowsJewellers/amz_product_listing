a212c78302fe91d51625f03f6d08167b3ba6a098
Amandeep Singh <amandeep@192.168.1.10>
2024-11-03 13:46:19 +0530

    Adjusted cron time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -60,8 +60,12 @@ class Kernel extends ConsoleKernel
         $schedule->command('getProductsFromEWebCatch')->everyFifteenMinutes()->between('02:00', '23:59');
         // $schedule->command('catchGenerateProductsCsv')->everyTwoHours()->between('02:00','23:59');
         $schedule->command('catchGenerateProductsCsv')->cron('18 2 */4 * *');
-        $schedule->command('catchGenerateOffersCsv')->everyFifteenMinutes()->between('02:00', '23:59');
-        $schedule->command('catchSubmitImports')->everyThirtyMinutes()->between('02:00', '23:59');
+        $schedule->command('catchGenerateOffersCsv')->everyFifteenMinutes()->between('02:00', '23:59')
+            ->after(function () {
+                $this->call('catchSubmitImports');
+            });
+
+        // $schedule->command('catchSubmitImports')->everyThirtyMinutes()->between('02:00', '23:59');
 
 
         /**
