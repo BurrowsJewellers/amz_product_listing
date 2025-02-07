b7e27fb099154f3d6023b8a7c17c66d2bb2e1cd8
Amandeep Singh <aman.dsj@gmail.com>
2025-02-07 12:51:11 +0530

    Renamed scheduler jobs

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,23 @@
 <?php
 
+use App\Console\Commands\Amazon\GetAmzMerchantListingAllData;
+use App\Console\Commands\Amazon\ProcessAmzMerchantListingAllData;
+use App\Console\Commands\Catch\CheckIfExists;
+use App\Console\Commands\Catch\GenerateOffersCsv;
+use App\Console\Commands\Catch\GenerateProductsCsv;
+use App\Console\Commands\Catch\GetProductsFromEWebCatch;
+use App\Console\Commands\Catch\ListOffersOfShop;
+use App\Console\Commands\Catch\SubmitImports;
 use App\Console\Commands\EWeb\GetProductsFromEWebMain;
+use App\Console\Commands\GetBrandsFromEWeb;
+use App\Console\Commands\Shopify\ArchiveProducts;
+use App\Console\Commands\Shopify\CountImages;
+use App\Console\Commands\Shopify\CreateProduct;
+use App\Console\Commands\Shopify\GetProducts;
+use App\Console\Commands\Shopify\UpdateInventory;
+use App\Console\Commands\Shopify\UpdatePrice;
+use App\Console\Commands\Shopify\UpdateProduct;
+use App\Console\Commands\Shopify\UploadImages;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
@@ -17,14 +34,14 @@ Schedule::call(GetProductsFromEWebMain::class)->everyFifteenMinutes()->after(fun
     Artisan::call('getProductsFromEWebAmazon'); //Amazon products
 });
 
-Schedule::call('getBrandsFromEWeb')->dailyAt('00:05');
+Schedule::call(GetBrandsFromEWeb::class)->dailyAt('00:05');
 
-Schedule::call('getAmzMerchantListingAllData')->everyThreeHours()->after(function () {
+Schedule::call(GetAmzMerchantListingAllData::class)->everyThreeHours()->after(function () {
     sleep(600);
-    Artisan::call('processAmzMerchantListingAllData');
+    Artisan::call(ProcessAmzMerchantListingAllData::class);
 });
 
-Schedule::call('processAmzMerchantListingAllData')->cron('32 */3 * * *');
+Schedule::call(ProcessAmzMerchantListingAllData::class)->cron('32 */3 * * *');
 
 
 /**
@@ -36,19 +53,19 @@ Schedule::call('processAmzMerchantListingAllData')->cron('32 */3 * * *');
 // Schedule::call('catchCheckIfExists')->dailyAt('00:50');
 // Schedule::call('catchListOffersOfShop')->dailyAt('01:20');
 
-Schedule::call('getProductsFromEWebCatch')->dailyAt('00:20')->after(function () {
-    $this->call('catchCheckIfExists');
+Schedule::call(GetProductsFromEWebCatch::class)->dailyAt('00:20')->after(function () {
+    $this->call(CheckIfExists::class);
 });
-Schedule::call('catchListOffersOfShop')->dailyAt('01:20');
+Schedule::call(ListOffersOfShop::class)->dailyAt('01:20');
 
 
 
-Schedule::call('getProductsFromEWebCatch')->everyFifteenMinutes()->between('02:00', '23:59');
+Schedule::call(GetProductsFromEWebCatch::class)->everyFifteenMinutes()->between('02:00', '23:59');
 // Schedule::call('catchGenerateProductsCsv')->everyTwoHours()->between('02:00','23:59');
-Schedule::call('catchGenerateProductsCsv')->cron('18 2 */4 * *');
-Schedule::call('catchGenerateOffersCsv')->everyFifteenMinutes()->between('02:00', '23:59')
+Schedule::call(GenerateProductsCsv::class)->cron('18 2 */4 * *');
+Schedule::call(GenerateOffersCsv::class)->everyFifteenMinutes()->between('02:00', '23:59')
     ->after(function () {
-        $this->call('catchSubmitImports');
+        $this->call(SubmitImports::class);
     });
 
 // Schedule::call('catchSubmitImports')->everyThirtyMinutes()->between('02:00', '23:59');
@@ -62,18 +79,18 @@ Schedule::call('catchGenerateOffersCsv')->everyFifteenMinutes()->between('02:00'
 //     $this->call('shopifyCreateProduct');
 // });
 
-Schedule::call('shopifyGetProducts')->cron("5 */2 * * *")->after(function () {
-    $this->call('shopifyCreateProduct');
+Schedule::call(GetProducts::class)->cron("5 */2 * * *")->after(function () {
+    $this->call(CreateProduct::class);
 });
 
 
 // Schedule::call('shopifyCreateProduct')->everyThreeHours();
 
-Schedule::call('shopifyUpdateInventory')->everyFifteenMinutes();
-Schedule::call('shopifyUpdatePrice')->everyFifteenMinutes();
-Schedule::call('shopifyUploadImages')->everyThreeHours();
-Schedule::call('shopifyArchiveProducts')->cron('20 */3 * * *');
+Schedule::call(UpdateInventory::class)->everyFifteenMinutes();
+Schedule::call(UpdatePrice::class)->everyFifteenMinutes();
+Schedule::call(UploadImages::class)->everyThreeHours();
+Schedule::call(ArchiveProducts::class)->cron('20 */3 * * *');
 
-Schedule::call('shopifyUpdateProduct')->cron('0 20 * * 6');
+Schedule::call(UpdateProduct::class)->cron('0 20 * * 6');
 
-Schedule::call('shopifyCountImages')->dailyAt('17:00');
+Schedule::call(CountImages::class)->dailyAt('17:00');
